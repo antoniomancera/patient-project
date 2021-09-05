4a6dec0d8a65e0d27cf279cb86380b59bca83af0
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2021-09-05 18:33:23 +0200

    fix: fix error in list-patient
    corrected the search with hapi fhir

--- src/app/list-patients/list-patients.component.ts
+++ src/app/list-patients/list-patients.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { HapiFhirServiceService } from '../shared/hapi-fhir-service.service';
+import { Patient } from '../shared/padientDto/patient';
 
 @Component({
   selector: 'app-list-patients',
   templateUrl: './list-patients.component.html',
-  styleUrls: ['./list-patients.component.css']
+  styleUrls: ['./list-patients.component.css'],
 })
 export class ListPatientsComponent implements OnInit {
+  patientListWithMedicationRequest: Patient[] = [];
+  patientListRandom: Patient[] = [];
+  idWithMedicationRequest: string = '1293520';
+  p: string = 'prueba';
 
-  constructor() { }
+  constructor(private hapiService: HapiFhirServiceService) {}
 
   ngOnInit(): void {
+    this.getPatientsById(this.idWithMedicationRequest);
+    this.getPatients();
+    this.patientListWithMedicationRequest.concat(this.patientListRandom);
+  }
+  getPatientsById(patientId: string) {
+    this.hapiService.getPatientsById(patientId).subscribe((data) => {
+      this.patientListWithMedicationRequest = data;
+      console.log(data);
+    });
   }
 
+  getPatients() {
+    this.hapiService.getRandomPatients().subscribe((data) => {
+      this.patientListRandom =
+        this.patientListWithMedicationRequest.concat(data);
+      console.log(data);
+    });
+  }
 }
