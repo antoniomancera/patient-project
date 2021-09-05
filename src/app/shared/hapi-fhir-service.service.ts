d40b0cd48c937100a3e357c948d27e855ec24740
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2021-09-05 18:28:47 +0200

    feat: change search for avoid null medication request
    now in every search the first patient have medication request available

--- src/app/shared/hapi-fhir-service.service.ts
+++ src/app/shared/hapi-fhir-service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Patient } from './padientDto/patient';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HapiFhirServiceService {
+  constructor(public http: HttpClient) {}
 
-  constructor() { }
+  public getPatientsById(patientId: string): Observable<Patient[]> {
+    return this.http
+      .get<any>('http://hapi.fhir.org/baseR4/Patient?_id=' + patientId)
+      .pipe(map((data) => data.entry));
+  }
+
+  public getRandomPatients(): Observable<Patient[]> {
+    return this.http
+      .get<any>('http://hapi.fhir.org/baseR4/Patient?')
+      .pipe(map((data) => data.entry));
+  }
 }
